e94899fdc638747ecb79e34abdaaa5c1f3b13623
yrina <hj3416song@naver.com>
2024-07-27 22:42:48 +0900

    version 2

--- pcap-test.c
+++ pcap-test.c
@@ -17,34 +17,37 @@ Param param = {
     .dev_ = NULL
 };
 
-struct libnet_ethernet_hdr {
-    u_int8_t  ether_dhost[6]; /* destination ethernet address */
-    u_int8_t  ether_shost[6]; /* source ethernet address */
-    u_int16_t ether_type;     /* protocol */
+struct eth_hdr {
+    uint8_t dst_mac[6];
+    uint8_t src_mac[6];
+    uint16_t type;
 };
 
-struct libnet_ipv4_hdr {
-    u_int8_t ip_vhl;          /* version << 4 | header length >> 2 */
-    u_int8_t ip_tos;          /* type of service */
-    u_int16_t ip_len;         /* total length */
-    u_int16_t ip_id;          /* identification */
-    u_int16_t ip_off;
-    u_int8_t ip_ttl;          /* time to live */
-    u_int8_t ip_p;            /* protocol */
-    u_int16_t ip_sum;         /* checksum */
-    struct in_addr ip_src, ip_dst; /* source and dest address */
+struct ipv4_hdr {
+    uint8_t header_len : 4;
+    uint8_t version : 4;
+    uint8_t type_of_service;
+    uint16_t total_packet_len;
+    uint16_t fragment_identification;
+    uint16_t flags;
+    uint8_t ttl;
+    uint8_t protocol;
+    uint16_t checksum;
+    uint8_t src_ip[4];
+    uint8_t dst_ip[4];
 };
 
-struct libnet_tcp_hdr {
-    u_int16_t th_sport;       /* source port */
-    u_int16_t th_dport;       /* destination port */
-    u_int32_t th_seq;         /* sequence number */
-    u_int32_t th_ack;         /* acknowledgement number */
-    u_int8_t th_offx2;        /* data offset, rsvd */
-    u_int8_t th_flags;        /* control flags */
-    u_int16_t th_win;         /* window */
-    u_int16_t th_sum;         /* checksum */
-    u_int16_t th_urp;         /* urgent pointer */
+struct tcp_hdr {
+    uint16_t src_port;
+    uint16_t dst_port;
+    uint32_t seq_num;
+    uint32_t ack_num;
+    uint8_t header_len : 4;
+    uint16_t flags : 12;
+    uint16_t window_size;
+    uint16_t checksum;
+    uint16_t urgent_ptr;
+    uint8_t data[20]; // Flexible array member for data
 };
 
 bool parse(Param* param, int argc, char* argv[]) {
@@ -52,24 +55,24 @@ bool parse(Param* param, int argc, char* argv[]) {
         usage();
         return false;
     }
-    param->dev_ = argv[1];
+    param->dev_ = argv[1];  
     return true;
 }
 
-void print_mac_address(const char* label, const u_int8_t* mac) {
+void print_mac_address(const char* label, const uint8_t* mac) {
     printf("%s %02x:%02x:%02x:%02x:%02x:%02x\n", label, mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
 }
 
-void print_ip_address(const char* label, struct in_addr ip) {
-    printf("%s %s\n", label, inet_ntoa(ip));
+void print_ip_address(const char* label, const uint8_t* ip) {
+    printf("%s %d.%d.%d.%d\n", label, ip[0], ip[1], ip[2], ip[3]);
 }
 
-void print_tcp_port(const char* label, u_int16_t port) {
+void print_tcp_port(const char* label, uint16_t port) {
     printf("%s %d\n", label, ntohs(port));
 }
 
-void print_payload(const u_char* payload, int len) {
-    printf("Payload: ");
+void print_payload(const uint8_t* payload, int len) {
+    printf("Payload (%d bytes): ", len);
     for (int i = 0; i < len && i < 20; ++i) {
         printf("%02x ", payload[i]);
     }
@@ -92,7 +95,7 @@ int main(int argc, char* argv[]) {
 
     while (true) {
         struct pcap_pkthdr* header;
-        const u_char* packet;
+        const uint8_t* packet;
         int res = pcap_next_ex(pcap, &header, &packet);
         if (res == 0) continue;
         if (res == PCAP_ERROR || res == PCAP_ERROR_BREAK) {
@@ -100,29 +103,34 @@ int main(int argc, char* argv[]) {
             break;
         }
 
-        struct libnet_ethernet_hdr* eth_hdr = (struct libnet_ethernet_hdr*)packet;
-        print_mac_address("Src MAC:", eth_hdr->ether_shost);
-        print_mac_address("Dst MAC:", eth_hdr->ether_dhost);
+        struct eth_hdr* eth_hdr = (struct eth_hdr*)packet;
+        print_mac_address("Src MAC:", eth_hdr->src_mac);
+        print_mac_address("Dst MAC:", eth_hdr->dst_mac);
 
-        if (ntohs(eth_hdr->ether_type) == 0x0800) { // IPv4
-            struct libnet_ipv4_hdr* ip_hdr = (struct libnet_ipv4_hdr*)(packet + sizeof(struct libnet_ethernet_hdr));
-            print_ip_address("Src IP:", ip_hdr->ip_src);
-            print_ip_address("Dst IP:", ip_hdr->ip_dst);
+        if (ntohs(eth_hdr->type) == 0x0800) { // IPv4
+            struct ipv4_hdr* ip_hdr = (struct ipv4_hdr*)(packet + sizeof(struct eth_hdr));
+            print_ip_address("Src IP:", ip_hdr->src_ip);
+            print_ip_address("Dst IP:", ip_hdr->dst_ip);
 
-            int ip_header_len = (ip_hdr->ip_vhl & 0x0F) * 4;
-            if (ip_hdr->ip_p == IPPROTO_TCP) {
-                struct libnet_tcp_hdr* tcp_hdr = (struct libnet_tcp_hdr*)((u_char*)ip_hdr + ip_header_len);
-                print_tcp_port("Src Port:", tcp_hdr->th_sport);
-                print_tcp_port("Dst Port:", tcp_hdr->th_dport);
+            int ip_header_len = ip_hdr->header_len * 4;
+            if (ip_hdr->protocol == IPPROTO_TCP) {
+                struct tcp_hdr* tcp_hdr = (struct tcp_hdr*)((uint8_t*)ip_hdr + ip_header_len);
+                print_tcp_port("Src Port:", tcp_hdr->src_port);
+                print_tcp_port("Dst Port:", tcp_hdr->dst_port);
 
-                int tcp_header_len = ((tcp_hdr->th_offx2 & 0xF0) >> 4) * 4;
-                const u_char* payload = (u_char*)tcp_hdr + tcp_header_len;
-                int payload_len = ntohs(ip_hdr->ip_len) - (ip_header_len + tcp_header_len);
-                print_payload(payload, payload_len);
+                int tcp_header_len = tcp_hdr->header_len * 4;
+                int eth_header_len = 14;
+                const uint8_t* payload = (uint8_t*)tcp_hdr->data;
+                int total_len = ntohs(ip_hdr->total_packet_len);
+                int header_len = ip_header_len + tcp_header_len + eth_header_len;                
+                int payload_len = total_len - header_len;
+                if (payload_len > 0) {
+                    print_payload(payload, payload_len);
+                } else {
+                    printf("No payload\n");
+                }
             }
         }
-
-        //printf("%u bytes captured\n", header->caplen);
     }
 
     pcap_close(pcap);
